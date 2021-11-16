2b0c3a0fa04332fe61bb9824e2901d626f275f24
Jonas Cabral <cabral.j@unoesc.edu.br>
2021-11-16 08:09:39 -0300

    estilizado pg index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Header from '../components/Header';
+import { FaRegUser, FaRegCalendar } from 'react-icons/fa';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,13 +27,42 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Jonas Blog</title>
+      </Head>
+      <Header />
+      <div className={styles.container}>
+        <section className={styles.content}>
+          <a href="#">
+            <h1 className={styles.title}>Como utilizar Hooks</h1>
+          </a>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+            reprehenderit necessitatibus incidunt eius quod odit illum aliquam
+            maiores laudantium, aliquid temporibus quisquam voluptatibus aut
+            minima ut quasi, accusantium magni expedita.
+          </p>
+        </section>
+
+        <section className={styles.info}>
+          <FaRegCalendar />
+          <time>15 Marc 2021</time>
+          <FaRegUser /> <strong>Joseph Oliveira</strong>
+        </section>
+        <a className={styles.more} href="#">
+          Carregar mais posts
+        </a>
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+//   const prismic = getPrismicClient();
+//   // const postsResponse = await prismic.query();
 
 //   // TODO
 // };
