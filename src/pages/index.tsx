3460f4783162613c70a8562bd4bfc6bdb8c9a04d
Jonas Cabral <cabral.j@unoesc.edu.br>
2021-11-16 18:16:49 -0300

    Erro ao usar RichText--> asText E asHtml

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,45 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
-import { FaRegUser, FaRegCalendar } from 'react-icons/fa';
-
+import { FaRegUser, FaRegCalendar, FaPooStorm } from 'react-icons/fa';
+import Prismic from '@prismicio/client';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import Link from 'next/link';
+// interface Post {
+//   uid?: string;
+//   first_publication_date: string | null;
+//   data: {
+//     title: string;
+//     subtitle: string;
+//     author: string;
+//   };
+// }
 
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
+// interface PostPagination {
+//   next_page: string;
+//   results: Post[];
+// }
+
+// interface HomeProps {
+//   postsPagination: PostPagination;
+// }
+type Post = {
+  slug?: string;
   data: {
-    title: string;
+    title: string | null;
     subtitle: string;
     author: string;
   };
-}
-
+  createdAt: string;
+};
 interface PostPagination {
-  next_page: string;
-  results: Post[];
+  next_page: string | null;
+  posts: Post[];
 }
 
-interface HomeProps {
-  postsPagination: PostPagination;
-}
-
-export default function Home() {
+export default function Home({ posts, next_page }: PostPagination) {
   return (
     <>
       <Head>
@@ -35,34 +47,71 @@ export default function Home() {
       </Head>
       <Header />
       <div className={styles.container}>
-        <section className={styles.content}>
-          <a href="#">
-            <h1 className={styles.title}>Como utilizar Hooks</h1>
-          </a>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
-            reprehenderit necessitatibus incidunt eius quod odit illum aliquam
-            maiores laudantium, aliquid temporibus quisquam voluptatibus aut
-            minima ut quasi, accusantium magni expedita.
-          </p>
-        </section>
+        {posts.map(post => (
+          <div key={post.slug}>
+            <section className={styles.content}>
+              <Link href={`/post/${post.slug}`}>
+                <a>
+                  <h1 className={styles.title}>{post.data.title}</h1>
+                </a>
+              </Link>
+              <p>{post.data.subtitle}</p>
+            </section>
 
-        <section className={styles.info}>
-          <FaRegCalendar />
-          <time>15 Marc 2021</time>
-          <FaRegUser /> <strong>Joseph Oliveira</strong>
-        </section>
+            <section className={styles.info}>
+              <FaRegCalendar />
+              <time>{post.createdAt}</time>
+              <FaRegUser /> <strong>{post.data.author}</strong>
+            </section>
+          </div>
+        ))}
         <a className={styles.more} href="#">
-          Carregar mais posts
+          {next_page !== null ? 'Carregar mais posts' : ''}
         </a>
       </div>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query();
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['results'],
+      pageSize: 20,
+    }
+  );
+  /**
+   * !! PARA VER TODO O CONTEÚDO QUE IMPORTAMOS DO PRISMIC
+   */
+  // console.log(JSON.stringify(postsResponse, null, 2));
+  // console.log(postsResponse.next_page);
+  const posts = postsResponse.results.map(post => {
+    return {
+      slug: post.uid,
 
-//   // TODO
-// };
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+      createdAt: new Date(post.first_publication_date).toLocaleDateString(
+        'pt-BR',
+        {
+          day: '2-digit',
+          month: 'short',
+          year: 'numeric',
+        }
+      ),
+    };
+  });
+  // console.log(posts);
+
+  return {
+    props: {
+      posts,
+      next_page: postsResponse.next_page,
+    },
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,34 +1,49 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetServerSideProps, GetStaticPaths, GetStaticProps } from 'next';
+import { RichText } from 'prismic-dom';
+import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
-interface Post {
-  first_publication_date: string | null;
-  data: {
+// interface Post {
+//   first_publication_date: string | null;
+//   data: {
+//     title: string;
+//     banner: {
+//       url: string;
+//     };
+//     author: string;
+//     content: {
+//       heading: string;
+//       body: {
+//         text: string;
+//       }[];
+//     }[];
+//   };
+// }
+
+// interface PostProps {
+//   post: Post;
+// }
+interface PostProps {
+  post: {
+    slug: string;
     title: string;
-    banner: {
-      url: string;
-    };
-    author: string;
-    content: {
-      heading: string;
-      body: {
-        text: string;
-      }[];
-    }[];
+    content: string;
+    updatedAt: string;
   };
 }
 
-interface PostProps {
-  post: Post;
+export default function Post({ post }: PostProps) {
+  return (
+    <div className={styles.container}>
+      <Header />
+      <h1>{post.slug}</h1>
+    </div>
+  );
 }
-
-// export default function Post() {
-//   // TODO
-// }
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
@@ -37,9 +52,30 @@ interface PostProps {
 //   // TODO
 // };
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+}) => {
+  const { slug } = params;
+  const prismic = getPrismicClient(req);
+  const response = await prismic.getByUID('post', String(slug), {});
+  console.log(response.data.title);
 
-//   // TODO
-// };
+  const post = {
+    slug,
+    title: RichText.asText(response.data.title),
+    // content: RichText.asHtml(response.data.content),
+    // createdAt: new Date(response.last_publication_date).toLocaleDateString(
+    //   'pt-BR',
+    //   {
+    //     day: '2-digit',
+    //     month: 'short',
+    //     year: 'numeric',
+    //   }
+    // ),
+  };
+
+  return {
+    props: { post },
+  };
+};
